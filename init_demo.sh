498feb1005f0f8cb41308dc9f6470043a448f740
Stefan Wolfsheimer <stefan.wolfsheimer@surfsara.nl>
2019-01-31 12:31:55 +0100

    added fake DOIs to init script

--- init_demo.sh
+++ init_demo.sh
@@ -8,6 +8,12 @@ sleep 30
 /build/b2share.py db init
 /build/b2share.py upgrade run -v
 sleep 20
+# create fake DOIs
+export B2SHARE_DEMO_CFG_FAIL_ON_MISSING_PID=1 
+export B2SHARE_DEMO_CFG_FAIL_ON_MISSING_DOI=1
+export B2SHARE_DEMO_FAKE_EPIC_PID=1
+export B2SHARE_DEMO_FAKE_DOI=1
+
 /build/b2share.py demo load_data
 
 
